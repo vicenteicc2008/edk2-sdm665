7d2f30d798e44d71c224af23a50721e8338a9ecc
Vicente Cortes <vicenteicc2008@gmail.com>
2023-10-27 16:17:29 -0300

    update build.sh script and remove Mi A2 fdt

--- build.sh
+++ build.sh
@@ -6,4 +6,14 @@ set -e
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -j$(nproc) -s -n 0 -a AARCH64 -t GCC5 -p MiA2Pkg/MiA2Pkg.dsc
 gzip -c < workspace/Build/MiA2Pkg/DEBUG_GCC5/FV/MIA2PKG_UEFI.fd >uefi_image
 cat ginkgo.dtb >>uefi_image
-mkbootimg --kernel uefi_image -o uefi.img
+python3 ./mkbootimg.py \
+  --kernel ./ImageResources/bootpayload.bin \
+  --ramdisk ./ImageResources/ramdisk \
+  --kernel_offset 0x00000000 \
+  --ramdisk_offset 0x00000000 \
+  --tags_offset 0x00000000 \
+  --os_version 13.0.0 \
+  --os_patch_level "$(date '+%Y-%m')" \
+  --header_version 1 \
+  -o uefi.img \
+  ||_error "\nFailed to create Android Boot Image!\n"
